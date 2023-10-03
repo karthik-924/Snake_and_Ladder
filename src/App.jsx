2f4cae39e63efa3fc6f24c1bdfb1ce7336b183ef
karthik-924 <20p61a1240@vbithyd.ac.in>
2023-10-03 22:31:19 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Button } from '@mantine/core'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p className=" text-blue-300">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button variant="filled">Button</Button>
+    </>
+  )
+}
+
+export default App
